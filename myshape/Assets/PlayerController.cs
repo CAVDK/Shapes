9094cc17cdfd95956a6d215c5c08630715ccbc5a
vedant <vedantk699@gamil.com>
2021-06-03 01:47:04 -0700

    prototype
    this is the basic idea ,start a new project from here on out

---
+++ myshape/Assets/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject[] shapes;
+    int i = 0;
+    int previousIndex;
+    public float moveSpeed = 3f;
+    public Vector2 offset;
+    Vector3 directionToMoveIn;
+    private void Start()
+    {
+        i = 0;
+        shapes[i].SetActive(true);
+        shapes[1].SetActive(false);
+        shapes[2].SetActive(false);
+    }
+   
+    
+    private void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            previousIndex = i;
+            shapes[previousIndex].SetActive(false);
+            i++;
+            if (i > shapes.Length - 1 || i < 0)
+            {
+                i = 0;
+              //  previousIndex = shap
+            }
+            
+            shapes[i].SetActive(true);
+            
+          
+           
+
+           
+
+        }
+
+        if(Input.GetButton("Fire1"))
+        {
+            Vector3 tempPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 _2dTempPosition = new Vector2(tempPosition.x, tempPosition.y); //+ offset;
+
+            // transform.position = Vector3.Lerp(transform.position, _2dTempPosition,Time.deltaTime*moveSpeed);
+            tempPosition.z = 0f;
+            directionToMoveIn = tempPosition - transform.position;
+
+        }
+        
+        transform.Translate(directionToMoveIn.normalized*moveSpeed * Time.deltaTime);
+
+    }
+   
+}
