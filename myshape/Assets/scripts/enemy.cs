2d4a2b83a402e5252c7fd788b02ecef3ae446ce9
vedant <vedantk699@gamil.com>
2021-06-05 07:39:30 -0700

    added  a basic layout
    i started the project alll over again,just
    cause i had too too many mistakes earlier
    and yeah insted of  removing everything i thought ,i should keep a few things so yeah we start again

---
+++ myshape/Assets/scripts/enemy.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    public float enemySpeed;
+
+    private void Update()
+    {
+        transform.Translate(Vector2.down * enemySpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+           // Debug.Log("playerTouch");
+        }
+    }
+}
